ca603d373355d2eef280bcaa56990fd800845434
RenugaDeviPalraj251003 <145784475+RenugaDeviPalraj251003@users.noreply.github.com>
2024-11-04 21:36:40 +0530

    Update Courses.jsx

--- src/pages/courses/Courses.jsx
+++ src/pages/courses/Courses.jsx
@@ -1,38 +1,4 @@
-/*import React from 'react';
-import "./courses.css";
-import { CourseData } from '../../context/CourseContext.jsx';
 
-const Courses = () => {
-
-  const {courses}=CourseData();
-  console.log(courses);
-  return   (
-    <div className="courses">
-
-    <h2>Available Courses</h2>
-    
-    <div className="course-container">
-    {courses && courses.length > 0 ? (
-
-courses.map((e) => <CourseCard key={e._id} course={e} />)
-
-):(
-
-<p>No Courses Yet!</p>)
-
-}
-  
-    
-    </div>
-    
- 
-    
-    </div>)
-  
-};
-
-export default Courses;
-*/
 import React from 'react';
 import "./courses.css";
  // Assuming CourseCard is in this path
